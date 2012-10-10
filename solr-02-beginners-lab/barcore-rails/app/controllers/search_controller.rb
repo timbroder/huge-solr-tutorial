f313a040b013724785d56cc69cf7d1f6327aad19
Toufique <toufique+github@gmail.com>
2012-10-10 13:32:34 -0400

    Adding spatial search.

--- solr-02-beginners-lab/barcore-rails/app/controllers/search_controller.rb
+++ solr-02-beginners-lab/barcore-rails/app/controllers/search_controller.rb
@@ -1,26 +1,31 @@
 class SearchController < ApplicationController
   
-  @@solr_url = 'http://localhost:8983/solr/barcore'
+  # @@solr_url = 'http://localhost:8983/solr/barcore'
+  @@solr_url = 'http://localhost:8080/solr-search/barcore'
   @@per_page = 25
 
   def index
     q = params[:q].blank? ? '*:*' : params[:q]
+    sort = params[:sort].blank? ? 'score desc, rating desc, price desc' : params[:sort]
+    pt = (params[:lat].blank? or params[:lon].blank?) ? '40.702902,-73.989748' : "#{params[:lat]},#{params[:lon]}"
 
     solr = RSolr::Ext.connect :url => @@solr_url
 
     solr_params = {
       queries: q,
+      sort: sort,
       start: @@per_page * (params[:page].nil? ? 0 : params[:page].to_i-1),
       rows: @@per_page,
       defType: :edismax,
       qf: 'name^3 cuisine^2 neighborhood city',
       facets: {fields: ['neighborhood', 'cuisine']},
-      sort: 'score desc, rating desc, price desc',
       'facet.mincount' => 1,
       hl: true,
       'hl.simple.pre' => '<strong><em><u>',
       'hl.simple.post' => '</u></em></strong>',
-      'sfield' => 'latLon',
+      sfield: 'location',
+      pt: pt,
+      fl: 'score,geodist(),*'
     }
 
     # Include any other request parameters
